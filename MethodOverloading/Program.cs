5a2bc99e29baa0f9c30b653956ea51b8f746e97f
Matteo DeSimone <thematteod@gmail.com>
2022-08-25 18:23:10 -0400

    Initial commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool isDollar)
+        {
+            if (isDollar)
+            {
+                if (a + b > 1 || a + b < -1)
+                {
+                    return $"{a + b} dollars";
+                }
+                else
+                {
+                    return $"{a + b} dollar";
+                }
+            }
+            else
+            {
+                return $"{a + b}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Add(1, 0, true);
+        }
+    }
+}
